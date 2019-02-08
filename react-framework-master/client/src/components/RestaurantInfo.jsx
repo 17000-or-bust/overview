5cc81914b7ec8b3b0e2750b8ba64122452c42357
Philip Stout <philipstout3@gmail.com>
2019-02-08 15:06:15 -0800

    Implement AJAX Post request

--- react-framework-master/client/src/components/RestaurantInfo.jsx
+++ react-framework-master/client/src/components/RestaurantInfo.jsx
@@ -9,8 +9,8 @@ class RestaurantInfo extends React.Component {
     return(
       <div>
         <div style={{display:'flex', paddingTop:"15px"}}>
-        {/* {this.props.doodles[2]} */}
-        <div style={this.props.boldText}>Cross street</div>
+        {this.props.doodles[this.props.index+3]}
+        <div style={this.props.boldText}>{this.props.title}</div>
       </div>
       <div style={this.props.subText}>{}</div>
     </div>
